c0c8201fb25b5a7209d5865c0794cc9d9087d813
jogonpav <josepabonvera@gmail.com>
2021-12-04 15:18:25 -0500

    Se carga proyecto inicial

---
+++ app/src/main/java/com/example/pokeapi/pokeapi/PokeapiService.java
@@ -0,0 +1,13 @@
+package com.example.pokeapi.pokeapi;
+
+import com.example.pokeapi.models.PokemonRespuesta;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface PokeapiService {
+
+    @GET("pokemon")
+    Call<PokemonRespuesta> obtenerListaPokemon (@Query("limit") int limit, @Query("offset") int offset);
+}
